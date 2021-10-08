4467c1f0fd1e8428b6fc69b922480358d5521b6b
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2021-10-08 22:13:00 +0700

    add file manager api

--- app/Http/Controllers/API/WorkPlan/FileManagerController.php
+++ app/Http/Controllers/API/WorkPlan/FileManagerController.php
@@ -7,30 +7,24 @@ use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\FileManager\FileManagerResource;
 use App\Models\FileManager;
-use App\Models\WorkPlan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rule;
 
 class FileManagerController extends Controller
 {
-    public function fetch(Request $request)
-    {
-        $this->validate($request, [
-            'work_plan_id' => ['required', 'exists:work_plans,id'],
-        ]);
-
-        $file_manager = FileManager::where('work_plan_id', $request->work_plan_id)->orderBy('id', 'desc')->get();
-        return ResponseFormatter::success(
-            FileManagerResource::collection($file_manager),
-            'success get file manager data'
-        );
-    }
-
     public function create(Request $request)
     {
         $this->validate($request, [
-            'work_plan_id' => ['required', 'exists:work_plans,id'],
+            'category' => ['required', 'in:work_plan,user_ro'],
+            'work_plan_id' => [
+                Rule::RequiredIf($request->category == 'work_plan'), 
+                'exists:work_plans,id'
+            ],
+            'user_ro_id' => [
+                Rule::RequiredIf($request->category == 'user_ro'), 
+                'exists:user_ro,id'
+            ],
             'file' => ['required', 'file'],
             'type_id' => [
                 'required',
@@ -41,17 +35,35 @@ class FileManagerController extends Controller
         ]);
 
         if($request->file('file')) {
-            $path = FileHelpers::upload_file('work_plan_file', $request->file, true);
+            if($request->category == 'work_plan') {
+                $path_file = 'work_plan_file';
+                $input['work_plan_id'] = $request->work_plan_id;
+            }  else {
+                $path_file = 'user_ro_file';
+                $input['user_ro_id'] = $request->user_ro_id;
+            }
+            $path = FileHelpers::upload_file($path_file, $request->file, true);
+            $input['file'] = $path;
+            $input['type_id'] = $request->type_id;
         }
 
-        $work_plan = WorkPlan::find($request->work_plan_id);
-        $file = $work_plan->file_manager()->create([ 
-            'file' => $path,
-            'type_id' => $request->type_id,
-        ]);
+        $file = FileManager::create($input);
         return ResponseFormatter::success(
             new FileManagerResource($file),
             'success upload file'
+        );
+    }
+
+    public function fetch(Request $request)
+    {
+        $this->validate($request, [
+            'work_plan_id' => ['required', 'exists:work_plans,id'],
+        ]);
+
+        $file_manager = FileManager::where('work_plan_id', $request->work_plan_id)->orderBy('id', 'desc')->get();
+        return ResponseFormatter::success(
+            FileManagerResource::collection($file_manager),
+            'success get file manager data'
         );
     }
 
