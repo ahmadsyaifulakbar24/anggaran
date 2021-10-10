702b281ffb733a162ec739d6935e1dc39330c839
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2021-10-10 18:43:25 +0700

    add unit target seeder

--- database/seeds/UnitTargetSeeder.php
+++ database/seeds/UnitTargetSeeder.php
@@ -13,9 +13,34 @@ class UnitTargetSeeder extends Seeder
      */
     public function run()
     {
-        UnitTarget::create([ 'name' => 'Layanan' ]);
+        UnitTarget::create([ 'name' => 'Dokumen' ]);
+        UnitTarget::create([ 'name' => 'Kegiatan' ]);
+        UnitTarget::create([ 'name' => 'Kebijakan' ]);
         UnitTarget::create([ 'name' => 'Koperasi' ]);
-        UnitTarget::create([ 'name' => 'Paket' ]);
+        UnitTarget::create([ 'name' => 'Lembaga' ]);
+        UnitTarget::create([ 'name' => 'Rancangan Peraturan' ]);
+        UnitTarget::create([ 'name' => 'Kesepakatan' ]);
+        UnitTarget::create([ 'name' => 'Provinsi' ]);
+        UnitTarget::create([ 'name' => 'Laporan' ]);
+        UnitTarget::create([ 'name' => 'Layanan' ]);
+        UnitTarget::create([ 'name' => 'Bulan' ]);
+        UnitTarget::create([ 'name' => 'Tahun' ]);
+        UnitTarget::create([ 'name' => 'Kelompok' ]);
+        UnitTarget::create([ 'name' => 'Kelompok Masyarakat' ]);
+        UnitTarget::create([ 'name' => 'Orang' ]);
+        UnitTarget::create([ 'name' => 'Rekomendasi Kebijakan' ]);
         UnitTarget::create([ 'name' => 'Unit' ]);
+        UnitTarget::create([ 'name' => 'PerMen' ]);
+        UnitTarget::create([ 'name' => 'Sistem Informasi' ]);
+        UnitTarget::create([ 'name' => 'Undang-Undang' ]);
+        UnitTarget::create([ 'name' => 'Daerah' ]);
+        UnitTarget::create([ 'name' => 'Usaha Mikro' ]);
+        UnitTarget::create([ 'name' => 'UKM' ]);
+        UnitTarget::create([ 'name' => 'Pelaku Usaha' ]);
+        UnitTarget::create([ 'name' => 'Start-Up' ]);
+        UnitTarget::create([ 'name' => 'Wirausaha' ]);
+        UnitTarget::create([ 'name' => 'Paket' ]);
+        UnitTarget::create([ 'name' => 'Promosi' ]);
+        UnitTarget::create([ 'name' => 'Kali' ]);
     }
 }
